664113083357bae6f31eb505848034f036c840f3
Synopsik <synopsikk@gmail.com>
2025-10-30 19:08:44 -0700

    updated word-guessing game for modularity in future testing.
    Created a few methods to handle the main events that will need to be handed. `startup()` resets the game and assigns a new random word, `take_guess()` ensures a single char is input from the user, and finally `register_guess()` completes the core logic to determine if the letter is correct and if we won/lose.

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,54 +1,89 @@
 import random
 import os, subprocess
+from typing import Any
+
+DEFAULT_GUESSES = 10
 
 
 class GuessGame:
     def __init__(self):
-        self._words = [
-            "apple","anchor","azure","brick","bridge","bubble","candle","canvas","cipher","clutch",
-            "clock","cloud","cobalt","comet","crisp","delta","dune","ember","epoch","echo",
-            "flint","forge","frame","glimmer","glyph","harbor","hatch","haze","hybrid","iris",
-            "ivory","jade","jolt","kernel","lumen","lark","matrix","meadow","mint","mosaic",
-            "nexus","nova","nimbus","oak","onyx","orbit","orchid","pivot","pixel","plume",
-            "quartz","quill","raven","ripple","rook","sable","saturn","scarlet","shift","silk",
-            "spire","sprite","summit","tango","tide","timber","token","tuple","umbra","urban",
-            "valve","vapor","vector","verdant","vex","vital","wisp","willow","wrath","xenon",
-            "yarn","yarrow","yield","zephyr","zinc","zen","spark","scale","byte","cache",
-            "agent","bytecode","cluster","daemon","engine","flask","grid","heap","index","lambda"
-        ]
+        self.is_over = False
+        self._words_list = ["cipher","epoch","glyph","kernel","matrix","nexus","pixel","vector","daemon","lambda"]
         self._random_word = ""
-        self.get_random_word()
-        self.start_guessing()
+        self._guess_word = ""
+        self._guesses = DEFAULT_GUESSES
 
 
-    def get_random_word(self) -> None:
-        if not self._words:
+    def get_words(self) -> list[str]:
+        return self._words_list
+
+
+    def get_random_word(self) -> str:
+        return self._random_word
+
+
+    def set_random_word(self) -> None:
+        if not self._words_list:
             raise Exception("No elements in words list")
-        self._random_word = random.choice(self._words)
+        self._random_word = random.choice(self._words_list)
 
 
-    def start_guessing(self) -> bool:
+    def can_guess(self) -> bool:
+        return self._guesses > 0
+
+
+    def startup(self) -> None:
         self._clear()
-        guess_word = ""
-        tries = 0
-        while True:
-            print(self._random_word)
-            print(f'Word = "{guess_word}"\n')
-            letter = input("Guess each letter: ")
-            if letter == self._random_word[len(guess_word)]:
-                guess_word += letter
+        self.set_random_word()
+        self._guess_word = ""
+        self._guesses = DEFAULT_GUESSES
+        self.is_over = False
+
+
+    def register_guess(self, letter) -> None:
+        if letter == self._random_word[len(self._guess_word)]:
+            self._guess_word += letter
+            if len(self._guess_word) == len(self._random_word):
                 self._clear()
-                if len(guess_word) == len(self._random_word):
-                    print("You win!")
-                    return True
-            else:
-                tries += 1
-                if tries > 10:
-                    print("You lose!")
-                    return False
-                print("\nIncorrect, try again!")
+                print("You win!")
+                print(f"Correct Word: {self._random_word}")
                 input("Press Enter to continue...")
+                self.is_over = True
+                return
+            # self._guesses = DEFAULT_GUESSES # Uncomment to refresh user guesses after correct guesses
+            self._clear()
+        else:
+            self._guesses -= 1
+            if not self.can_guess():
                 self._clear()
+                print("You lose!")
+                input("Press Enter to continue...")
+                self.is_over = True
+                return
+            print("\nIncorrect, try again!")
+            input("Press Enter to continue...")
+            self._clear()
+
+
+
+    def take_guess(self) -> str:
+        while True:
+            print("Welcome to the word-guessing game!")
+            print(f"Remaining Guesses: {self._guesses}")
+            # print(f"Random Word: {self._random_word}") # Uncomment to show random word to the user
+
+            if len(self._guess_word) > 0:
+                print(f"\nCurrent Guess: {self._guess_word}")
+
+            guess = input("\nEnter a single letter: ")
+            if (guess.isalpha()
+                    and isinstance(guess, str)
+                    and len(guess) == 1):
+                return guess
+
+            print("\nError! Type only a single alphabetic letter to continue.")
+            input("Press Enter to continue")
+            self._clear()
 
 
     @staticmethod # If you don't need to use `self` anywhere in your method
@@ -67,6 +102,12 @@ class GuessGame:
 
 def main() -> None:
     game = GuessGame()
+
+    while True:
+        game.startup()
+        while not game.is_over:
+            guess = game.take_guess()
+            game.register_guess(guess)
 
 
 if __name__ == "__main__":
